fb918a1a3bfb49ada632ff4b1997f6086f1e2c18
fadeaway-41 <cityboi6741d@gmail.com>
2026-07-06 11:40:02 +0200

    added mp3 to codecs

--- src/utils/codes.tsx
+++ src/utils/codes.tsx
@@ -8,4 +8,6 @@ export const CODECS: Record<string, string[]> = {
   mkv: ["-c:v", "libx264", "-c:a", "aac"],
 
   webm: ["-c:v", "libvpx-vp9", "-c:a", "libopus"],
+
+  mp3: ["-c:a", "libmp3lame", "-q:a", "2"],
 };
