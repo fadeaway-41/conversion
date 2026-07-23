f84a7bf7ff5e24735df9610380365bf13bebca40
fadeaway-41 <cityboi6741d@gmail.com>
2026-07-23 14:44:24 +0200

    improved css

--- src/dropper/conversion.tsx
+++ src/dropper/conversion.tsx
@@ -54,7 +54,11 @@ export default  function Dropper()
             </div>
             <div className="inputs d-flex  align-items-center">
             <div className="select-wrapper">
-                <button className="select-button" onClick={() => setIsOpen(!isOpen)}>{newtype.toUpperCase()}</button>
+                <button className="select-button" onClick={() => setIsOpen(!isOpen)}>{newtype.toUpperCase()}
+                    <svg className="select-arrow" width="15" height="15" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.5 4.5L6 8L9.5 4.5" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </button>
                 {isOpen && (
                     <div className="select-menu">
                         {formats.map(option =>
