24c5c6a7dff43c043d66e94b636f900f1e1ef987
fadeaway-41 <cityboi6741d@gmail.com>
2026-07-23 14:07:44 +0200

    improved css and html

--- src/dropper/conversion.tsx
+++ src/dropper/conversion.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState , useRef } from "react";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { CodecConversion } from "../utils/fileUtils.tsx";
 import { UrlConversion } from "../utils/UrlConversion.tsx";
+import { CODECS } from "../utils/codes.tsx";
 import Nav from "../elements/nav/nav.jsx"
 import UploadBox from "../elements/uploadbox/uploadbox.jsx";
 export default  function Dropper()
@@ -44,51 +45,42 @@ export default  function Dropper()
         // ffmpeg -i "YOUR URL TO DOWNLOAD VIDEO FROM" -c:v libx264 -preset slow -crf 22 "saveas.mp4"
     }
     return (
-<div className="vh-100 main-container w-100">
+    <div className="vh-100 main-container w-100">
         <Nav />
 
-        <div className="center-box d-flex justify-content-center align-items-center">
+        <div className="center-box d-flex justify-content-center align-items-center flex-column mt-5">
             <div className="d-flex box flex-column">
                 <UploadBox handleFileChange={handleFileChange} />
             </div>
-        </div>
-
-        <div className="inputs h-25 d-flex flex-column align-items-center">
-            <label className="input-label text-white mb-2">
-                Output format
-            </label>
-
+            <div className="inputs d-flex  align-items-center">
             <div className="select-wrapper">
-            <select
-                    onChange={(e) => {
-                        setNewtype(e.target.value);
-                    }}
-                    className="custom-select d-flex"
-                >
-                    <option value="avi">AVI</option>
-                    <option value="mov">MOV</option>
-                    <option value="mp4">MP4</option>
-                    <option value="mkv">MKV</option>
-                    <option value="webm">WEBM</option>
-                </select>
+                <select
+                        onChange={(e) => {
+                            setNewtype(e.target.value);
+                        }}
+                        className="custom-select d-flex text-white align-items-center"
+                        >
+                        <option value="avi">AVI</option>
+                        <option value="mov">MOV</option>
+                        <option value="mp4">MP4</option>
+                        <option value="mkv">MKV</option>
+                        <option value="webm">WEBM</option>
+                    </select>
+                </div>
+    {/* 
+                <input
+                    type="text"
+                    className="w-25 form-control mt-3"
+                    onChange={(e) => setNewtype(e.target.value)}
+                /> */}
 
-                <span className="select-arrow">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 9L12 15L18 9" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg>
-                </span>
+                <button className="w-25 btn btn-light mt-3" onClick={handleUrlChange}>
+                    Click
+                </button>
             </div>
-{/* 
-            <input
-                type="text"
-                className="w-25 form-control mt-3"
-                onChange={(e) => setNewtype(e.target.value)}
-            /> */}
-
-            <button className="w-25 btn btn-light mt-3" onClick={handleUrlChange}>
-                Click
-            </button>
         </div>
+
+        
     </div>
     )
 }
