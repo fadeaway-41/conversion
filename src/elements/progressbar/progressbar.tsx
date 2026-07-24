68c15a71a1a97f1cbf4afa727acc676475fad24c
fadeaway-41 <cityboi6741d@gmail.com>
2026-07-24 16:08:09 +0200

    added progressbar element folder

---
+++ src/elements/progressbar/progressbar.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import "../progressbar/progressbar.css";    
+
+interface ProgressBarProps {
+    progress: number;
+}
+
+export default function ProgressBar({ progress }: ProgressBarProps) {
+    const [displayProgress, setDisplayProgress] = useState(0);
+
+    useEffect(() => {
+    const intervalid = setInterval(() => {
+        setDisplayProgress(prev => {
+            if (prev >= progress) return prev;
+            return Math.min(prev + 1, progress);
+        });
+    }, 16);
+    console.log(displayProgress)
+    return () => clearInterval(intervalid);
+    }, [progress]);
+
+    return (
+        <div className="progressbar">
+            <div className="progress" style={{ width: ((displayProgress) + "%")}}>
+            </div>
+        </div>
+    );
+}
