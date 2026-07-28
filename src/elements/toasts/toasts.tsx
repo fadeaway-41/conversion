9c6c3451983a93e916acef1ada4240890bc1c797
fadeaway-41 <cityboi6741d@gmail.com>
2026-07-28 12:57:31 +0200

    added custom messages for toasts through props passing

--- src/elements/toasts/toasts.tsx
+++ src/elements/toasts/toasts.tsx
@@ -12,7 +12,6 @@ interface NotifyProps
 
 const Notify = ({appear,setAppear,variant,message} : NotifyProps) =>
 {
-    console.log(variant);
     // const filename = file.name.split('.')[0];
     const headermsg = message.split('/')[0];
     const bodymsg = message.split('/')[1];
@@ -20,7 +19,7 @@ return (
     <>
         {['Success','Danger'].filter(item => item === variant).map(item => 
             <ToastContainer style={{ zIndex: 1 }}>
-            <Toast show={appear} delay={4000} autohide bg={item.toLowerCase()} onClose={() => setAppear(false)}>
+            <Toast show={appear} delay={5000} autohide bg={item.toLowerCase()} onClose={() => setAppear(false)}>
             <Toast.Header>
                 { 
                     item == 'Success' ? <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="currentColor" className="text-success rounded me-2" ><path fillRule="evenodd" clipRule="evenodd" d="M10 0C4.477 0 0 4.477 0 10s4.477 10 10 10 10-4.477 10-10S15.523 0 10 0zm4.03 7.03l-4.5 4.5a.75.75 0 01-1.06 0l-2-2a.75.75 0 111.06-1.06l1.47 1.47 3.97-3.97a.75.75 0 111.06 1.06z"/></svg> 
